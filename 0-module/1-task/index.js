21b168d59f98d09906a1016c14c3bf684a1e8310
Aziza Budaeva <budaevaaziza@yandex.ru>
2020-09-02 15:13:50 +0300

    Solution task
    A function that sums two numbers.

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,9 +1,14 @@
+//Aziza Budaeva
 /**
  * sum
  * @param {number} m
  * @param {number} n
  * @returns {number}
  */
+ 
 function sum(m, n) {
-  // ваш код...
-}
+  return m+n;
+  }
+  let result = sum (2, 7);
+  alert(result);
+
